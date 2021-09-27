f5f7196ba44ecc89e023b31a9b2a459d0d2a0f01
reedleoneil <reedleoneilpascual@yahoo.com>
2021-09-27 23:08:53 +0800

    fixed pvp bugs

--- pvp.js
+++ pvp.js
@@ -1,9 +1,12 @@
 let pvpModule = {
+    playerName: "",
     roomid: null,
     combatStatus: 0,
     client: mqtt.connect('wss://test.mosquitto.org:8081'),
     onLoad: (options) => {
         console.log('loaded: ' + options);
+
+        pvpModule.playerName = options.playerName;
 
         pvpModule.client.on("message", (topic, payload) => {
             try {
@@ -13,6 +16,9 @@ let pvpModule = {
                 }
                 else if (topic.includes('getMoveResult')) {
                     options.getMoveResult(message);
+                }
+                else if (topic.includes('onJoin')) {
+                    options.onJoin(message);
                 }
             }
             catch(err) {
@@ -25,11 +31,14 @@ let pvpModule = {
     joinRoom: (room) => {
         pvpModule.client.subscribe('nft-poc-game/rooms/' + room + '/addEditProfile');
         pvpModule.client.subscribe('nft-poc-game/rooms/' + room + '/getMoveResult');
+        pvpModule.client.subscribe('nft-poc-game/rooms/' + room + '/onJoin');
         pvpModule.roomid = room;
+        pvpModule.client.publish('nft-poc-game/rooms/' + room + '/onJoin', JSON.stringify({ Name: pvpModule.playerName }));
     },
     leaveroom: (room) => {
         pvpModule.client.unsubscribe('nft-poc-game/rooms/' + room + '/addEditProfile');
         pvpModule.client.unsubscribe('nft-poc-game/rooms/' + room + '/getMoveResult');
+        pvpModule.client.unsubscribe('nft-poc-game/rooms/' + room + '/onJoin');
         pvpModule.roomid = null;
     },
     addEditProfile: (profile) => {
@@ -37,5 +46,9 @@ let pvpModule = {
     },
     getMoveResult: (moves) => {
         pvpModule.client.publish('nft-poc-game/rooms/' + pvpModule.roomid + '/getMoveResult', JSON.stringify(moves));
+    },
+    onJoin: () => {
+        pvpModule.client.unsubscribe('nft-poc-game/rooms/' + pvpModule.roomid + '/onJoin');
+        pvpModule.client.publish('nft-poc-game/rooms/' + pvpModule.roomid + '/onJoin', JSON.stringify({ Name: pvpModule.playerName }));
     }
 }
